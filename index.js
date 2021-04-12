860614ad3eb22bc522cdd83810f3fb9600993dad
Ashlee <ashleeryan@outlook.com>
2021-04-12 20:18:37 +0100

    added mongoose

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express()
+app.use()
+
+// connect to mongodb
+const dbURI = "mongodb+srv://administrator:motoreasy123@cluster0.keb60.mongodb.net/car-parts?retryWrites=true&w=majority"
