661de9083af18becd4c7960cf1ce7524b326d657
Ashlee <ashleeryan@outlook.com>
2021-04-12 21:56:06 +0100

    creates Schemas for brands and tyres

--- models/tyres.js
+++ models/tyres.js
@@ -1,1 +1,37 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const tyresSchema = new Schema({
+    brand:{
+        type: String,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    size:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required:true
+    },
+    image:{
+        type:String
+    }
+})
+
+const brandsSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    logo:{
+        type: String,
+        
+    }
+    
+})
