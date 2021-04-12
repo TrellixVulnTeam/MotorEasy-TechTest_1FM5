6a4d8d9a10f92105aebb9227a1b50677a4467e96
Ashlee <ashleeryan@outlook.com>
2021-04-12 20:26:03 +0100

    Created models folder and tyres.js for schema

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 const app = express()
-app.use()
+app.use(express.json())
 
 // connect to mongodb
 const dbURI = "mongodb+srv://administrator:motoreasy123@cluster0.keb60.mongodb.net/car-parts?retryWrites=true&w=majority"
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    app.listen(3000)
+    console.log("connected to db")
+}).catch((err) => console.log("err"))
+
+
