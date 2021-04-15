9c061dbde8ceddf0ed5c8f13e261434f26fb49ae
Ashlee <ashleeryan@outlook.com>
2021-04-15 20:38:40 +0100

    Created jest test that tests rendering of NavBar header

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NavBar from './components/NavBar'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('Render testing', () => {
+
+  test('Render Testing', () => {
+    const { getByText } = render(<NavBar />);
+
+    const linkElement = getByText("Tyres Direct");
+
+    expect(linkElement).toBeInTheDocument();
+  });
+
+})
+
